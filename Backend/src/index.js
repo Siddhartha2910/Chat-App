8fb939a369915b108f4d85b26a1076aa8bdc3639
Gade Siddhartha Kumar <siddharthakumargade2910@gmail.com>
2025-07-19 16:15:31 +0530

    Final attempt to fix path-to-regexp error by re-typing static serving

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -32,10 +32,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // UNCOMMENT THIS ENTIRE BLOCK
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"..","Frontend","dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "..", "Frontend", "dist", "index.html"));
+
     });
 }
 
